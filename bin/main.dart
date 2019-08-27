43e8cc49f9c1988f5f802edb97e0db93582e4f46
Banee Ishaque K <baneeishaque@gmail.com>
2019-08-27 13:25:01 +0530

    In

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:Hello_World_Dart/Hello_World_Dart.dart' as hello_world_dart;
+
+main(List<String> arguments) {
+  print('Hello world: ${hello_world_dart.calculate()}!');
+}
---
+++ test/Hello_World_Dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:Hello_World_Dart/Hello_World_Dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
